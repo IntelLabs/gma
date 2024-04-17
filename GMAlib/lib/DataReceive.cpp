4ebd48f4c8cf65efb1794a2abf9af838e60256d6
Jing <jingzhu77@gmail.com>
2024-04-17 09:27:36 -0700

    minor update

--- GMAlib/lib/DataReceive.cpp
+++ GMAlib/lib/DataReceive.cpp
@@ -515,7 +515,7 @@ void DataReceive::receiveLteControl(char *packet)
                      if (tscMessage.getDLDynamicSplittingEnabled() == 4) //disable fast loss detection
                       {
                         p_systemStateSettings->FAST_LOSS_DETECTION = false; 
-                        printf("\n fast loss detection disbaled\n");
+                        printf("\n fast loss detection disabled\n");
                       }
                      else 
                      {
